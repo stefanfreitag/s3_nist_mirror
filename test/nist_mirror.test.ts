a6ef3993a5850ac74c215455124ab429fedc7c4b
Stefan Freitag <stefan.freitag@udo.edu>
2020-08-31 19:08:27 +0200

    fix: fixed broken test

--- test/nist_mirror.test.ts
+++ test/nist_mirror.test.ts
@@ -1,13 +1,27 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import cdk = require('@aws-cdk/core');
-import NistMirror = require('../lib/nist_mirror-stack');
+import {
+  expect as expectCDK,
+  matchTemplate,
+  MatchStyle,
+  haveResource,
+  haveResourceLike,
+} from "@aws-cdk/assert";
+import cdk = require("@aws-cdk/core");
+import { Stack } from "@aws-cdk/core";
+import { NistMirrorStack } from "../lib/nist_mirror-stack";
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new NistMirror.NistMirrorStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+test("S3 bucket is not public accessible ", () => {
+  const stack = new Stack();
+
+  const s = new NistMirrorStack(stack, "stack");
+
+  expectCDK(s).to(
+    haveResourceLike("AWS::S3::Bucket", {
+      PublicAccessBlockConfiguration: {
+        BlockPublicAcls: true,
+        BlockPublicPolicy: true,
+        IgnorePublicAcls: true,
+        RestrictPublicBuckets: true,
+      },
+    })
+  );
 });

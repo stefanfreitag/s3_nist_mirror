9133ee95491bfbbc3dcbae37cfa347c1ffd28ef8
Stefan Freitag <stefan.freitag@udo.edu>
2023-09-15 21:13:51 +0200

    chore: upgrade dependencies

--- .projenrc.js
+++ .projenrc.js
@@ -4,14 +4,14 @@ const { UpgradeDependenciesSchedule } = require('projen/lib/javascript');
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Stefan Freitag',
   authorAddress: 'stefan.freitag@udo.edu',
-  cdkVersion: '2.73.0',
+  cdkVersion: '2.96.2',
   stability: Stability.EXPERIMENTAL,
   defaultReleaseBranch: 'master',
   name: 's3_nist_mirror',
   repositoryUrl: 'git://github.com/stefanfreitag/s3_nist_mirror.git',
   depsUpgradeOptions: {
     workflowOptions: {
-      schedule: UpgradeDependenciesSchedule.WEEKLY,
+      schedule: UpgradeDependenciesSchedule.MONTHLY,
     },
   },
   keywords: [
@@ -19,7 +19,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
     'cdk',
     's3',
   ],
-
+  typescriptVersion: '4.3.5',
 });
 project.gitignore.addPatterns('.history/');
 project.npmignore.addPatterns('.history/');
